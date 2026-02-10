f903c11042444de2dddb751e7630a8e6bb89ea12
Ell Wilkinson <elly.wilkinson1@gmail.com>
2026-02-10 18:24:22 +0000

    Added basic routing

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+
+    { path: '', component: HomeComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: 'portfolio', component: PortfolioComponent}
+];
